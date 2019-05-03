fb25701b6fd3f3b0e416f81c6cc1aca0cfe00750
Nestorferrando <ferrando.nestor@gmail.com>
2019-05-03 07:46:24 +0200

    implemented missing endpoints, not tested on iphone yet

---
+++ depthmapserver/Assets/scripts/DepthReadResult.cs
@@ -0,0 +1,19 @@
+﻿
+namespace UniWebServer
+{
+
+    public class DepthReadResult
+    {
+        public float[] data;
+        public int width;
+        public int height;
+
+        public DepthReadResult(float[] data, int width, int height)
+        {
+            this.data = data;
+            this.width = width;
+            this.height = height;
+        }
+    }
+
+}
