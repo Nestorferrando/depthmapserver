03d1f2a71e803e8cbf20fac2f1ddf6ea12c51180
Nestorferrando <ferrando.nestor@gmail.com>
2019-05-05 06:09:19 +0200

    added depthgetter project

--- depthgetter/src/Main.kt
+++ depthgetter/src/Main.kt
@@ -1,12 +1,36 @@
+import java.io.File
 import java.net.HttpURLConnection
 import java.net.URL
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 
 fun main(args: Array<String>) {
-    val data = sendGet("127.0.0.1", 8079, "depthdata")
-    println(data)
 
+    val address = if (args.isNotEmpty()) args[0] else "127.0.0.1"
+    val targetFile = if (args.size>1) args[1] else "iphoneDeph-${LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH.mm.ss"))}.json"
+    val data = sendGet(address, 8079, "depthdata") ?: throw RuntimeException("Received no response from server")
+
+    /*
+    //test stuff
+    val response =Gson().fromJson(data,Response::class.java)
+    val img = Base64.getDecoder().decode(response.jpgImageData)
+    val depth = Base64.getDecoder().decode(response.depthData)
+    val buffer = ByteBuffer.wrap(depth)
+    buffer.order( ByteOrder.LITTLE_ENDIAN)
+    val float2 = buffer.getFloat(0)
+    println(float2)
+    File("image.jpg").writeBytes(img)*/
+
+    val file = File(targetFile)
+    File(File(file.absolutePath).parent).mkdirs()
+    file.writeText(data)
 }
+
+
+data class Response(val depthData: String,val depthWidth: Int,val depthHeight: Int,val jpgImageData: String)
 
 fun sendGet(address: String, port: Int, endPoint: String): String? {
     val url = URL("http://$address:$port/$endPoint")
